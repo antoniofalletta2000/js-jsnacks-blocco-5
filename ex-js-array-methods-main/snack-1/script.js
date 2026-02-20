416d359e44d9ab668834465d36cbbb2165332bf6
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-02-20 13:28:18 +0100

    complete first snack

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -3,3 +3,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+    console.log(names.join(","));
+    
+    
+    
+
